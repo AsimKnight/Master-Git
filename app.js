7a6a1b37a4e11d64ebfb04b13e572279218f623d
Asim Shaikh <shaikhasimofficial@gmail.com>
2026-06-19 23:18:50 +0530

    8 contact apge added

--- app.js
+++ app.js
@@ -9,3 +9,5 @@ console.log("5 footer created");
 console.log("6 payment gateway implemented by shaikh");
 
 console.log("7 lazy loading implemented by shaikh");
+
+console.log("8 conatact page added");
